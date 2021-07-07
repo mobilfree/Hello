9317c96fe95bf9007a7d407d704a25fffb920c52
Artem Shchegolev <mobilfree@mail.ru>
2021-07-07 18:41:15 +0300

    Initial Commit

---
+++ Hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hello
+//
+//  Created by Artem on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
